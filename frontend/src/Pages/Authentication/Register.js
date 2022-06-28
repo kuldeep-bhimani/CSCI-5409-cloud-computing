a19c8d5cc703bd374f4d122a45908dd2a8c1de74
kishan mahendrabhai savaliya <ks317715@dal.ca>
2022-06-28 16:17:39 -0300

    authentication updates

--- frontend/src/Pages/Authentication/Register.js
+++ frontend/src/Pages/Authentication/Register.js
@@ -1,100 +1,185 @@
 import React, { useState } from "react";
-import DatePicker from "react-datepicker";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+//import FormControlLabel from "@mui/material/FormControlLabel";
+//import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+import { LocalizationProvider, DatePicker } from "@mui/lab";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import { useNavigate, useLocation } from "react-router-dom";
 // UserPool
 import UserPool from "../../UserPool/UserPool";
 
-// css
-import "../../css/registration.css";
-import "react-datepicker/dist/react-datepicker.css";
+// important for cloud
 import { CognitoUserAttribute } from "amazon-cognito-identity-js";
 
-const Register = () => {
-  const [birthdate, setBirthdate] = useState(null);
+const theme = createTheme();
+
+export default function Register() {
+  const [birthdate, setBirthdate] = useState(new Date());
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [userData, setUserData] = useState([]);
+  // Navigator
+  const navigate = useNavigate();
 
-  const submitHandler = (e) => {
+  //location
+  const location = useLocation();
+  // Submit Handler
+  const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(name, email, password, birthdate);
+    // Setting attribute
     const nameAttribute = new CognitoUserAttribute({
       Name: "name",
       Value: name,
     });
-    const dateAttribute = new CognitoUserAttribute({
-      Name: "birthdate",
-      Value: birthdate,
-    });
-    console.log("Handled.", email, password);
-    UserPool.signUp(
+    // const birthdateAttribute = new CognitoUserAttribute({
+    //   Name: "birthdate",
+    //   Value: birthdate,
+    // });
+
+    // registering user to AWS Cognito
+    const cognitoUser = UserPool.signUp(
       email,
       password,
-      [nameAttribute, dateAttribute],
+      [nameAttribute],
       null,
       (err, data) => {
         if (err) {
-          console.log("ERROR==>", err);
+          console.log("Error:", err);
         }
         if (data) {
-          console.log("DATA==>", data);
+          console.log("Data:", typeof data.user);
+          setUserData(data.user);
+          localStorage.setItem("username", data.user.username);
+          navigate("/login");
         }
       }
     );
   };
+
   return (
-    <div className="regi-wrapper">
-      <form onSubmit={(e) => submitHandler(e)}>
-        <div className="form-group regi-comp">
-          <label htmlFor="name">Name</label>
-          <input
-            type="test"
-            className="form-control"
-            id="name"
-            aria-describedby="emailHelp"
-            placeholder="Enter name"
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: "url(https://source.unsplash.com/random)",
+            backgroundRepeat: "no-repeat",
+            backgroundColor: (t) =>
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Register
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="name"
+                label="Name"
+                name="name"
+                autoComplete="name"
+                autoFocus
+                onChange={(e) => setName(e.target.value)}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
 
-        <div className="form-group regi-comp">
-          <label htmlFor="email">Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            aria-describedby="emailHelp"
-            placeholder="Enter email"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="form-group regi-comp">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            className="form-control"
-            id="password"
-            placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div className="form-group regi-comp">
-          <label htmlFor="birthdate">Birthdate</label>
-          <DatePicker
-            selected={birthdate}
-            onChange={(date) => setBirthdate(date)}
-            maxDate={new Date()}
-            dateFormat="MM/dd/yyyy"
-            placeholderText="MM/dd/yyyy"
-            showYearDropdown
-            scrollableMonthYearDropdown
-          />
-        </div>
-
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
-    </div>
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DatePicker
+                  label="Birth Date"
+                  renderInput={(params) => <TextField {...params} />}
+                  value={birthdate}
+                  onChange={(date) => {
+                    setBirthdate(date);
+                  }}
+                  margin="normal"
+                />
+              </LocalizationProvider>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Register
+              </Button>
+              <Grid container>
+                {/* <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid> */}
+                <Grid item>
+                  <Link href="/login" variant="body2">
+                    {"Already have an account? Login"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   );
-};
-
-export default Register;
+}
